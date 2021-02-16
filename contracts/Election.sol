bb17e2144ced42252bf4d0906eeab5e183b1f2d3
Priyansh <Priyansh.com>
2021-02-16 13:15:38 +0530

    1.SmokeTest

---
+++ contracts/Election.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.0;
+
+contract Election{
+	
+	//Store Name
+	//Read candidate
+	string public candidate;
+
+	//Constructor
+	constructor() public {
+
+		candidate = "Candidate 1";
+
+	}
+	
+}
